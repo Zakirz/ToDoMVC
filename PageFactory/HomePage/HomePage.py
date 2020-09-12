3669d9807a361dfda0160d509b296b1d4768aaec
ZakirIndium <66434705+ZakirIndium@users.noreply.github.com>
2020-09-12 14:03:47 +0530

    completing tests for a run

--- PageFactory/HomePage/HomePage.py
+++ PageFactory/HomePage/HomePage.py
@@ -1,6 +1,7 @@
 from Core.BrowserHelpers import BrowserHelpers
 from . import homepage_or
 from selenium.webdriver.common.keys import Keys
+import time
 
 
 class HomePage(BrowserHelpers):
@@ -9,9 +10,34 @@ class HomePage(BrowserHelpers):
 
     def add_todo(self, todo):
         self.enter_text(self.obj.add_new_todo, todo)
-        self.enter_text(self.obj.add_new_todo, Keys.ENTER)
+        self.enter_key(self.obj.add_new_todo, Keys.ENTER)
         self.wait_time(2)
 
-    def is_todo_added(self, todo):
+    def is_todo_present(self, todo):
         todo_locator = self.obj.select_task.replace("TASK_PLACEHOLDER", todo)
         return self.is_element_present(todo_locator)
+
+    def check_todo(self, todo):
+        todo_locator = self.obj.check_task.replace("TASK_PLACEHOLDER", todo)
+        self.click_on(todo_locator)
+
+    def is_todo_checked(self, todo):
+        todo_locator = self.obj.checked_task.replace("TASK_PLACEHOLDER", todo)
+        return self.is_element_present(todo_locator)
+
+    def get_todo_count(self):
+        return self.get_text(self.obj.task_count)
+
+    def delete_todo(self, todo):
+        todo_locator = self.obj.select_task.replace("TASK_PLACEHOLDER", todo)
+        todo_remove_locator = self.obj.delete_task.replace(
+            "TASK_PLACEHOLDER", todo)
+        self.mouse_hover(todo_locator)
+        time.sleep(1)
+        self.click_on(todo_remove_locator)
+
+    def click_active_filter(self):
+        self.click_on(self.obj.filter_active)
+
+    def click_completed_filter(self):
+        self.click_on(self.obj.filter_completed)
--- UI_Tests/Test_Homepage.py
+++ UI_Tests/Test_Homepage.py
@@ -7,6 +7,35 @@ class Test_Homepage(BrowserHelpers):
 
     def test_todomvc(self):
         self.navigate_to_url(self.BASE_URL)
-        
-        self.homepage.add_todo("Zakir")
-        assert self.homepage.is_todo_added("Zakir")
+        first_task = "Drink water every hour"
+
+        # Add to-do is working
+        self.homepage.add_todo(first_task)
+        assert self.homepage.is_todo_present(first_task)
+
+        # Marking an item as complete works.
+        inactive_task_count = self.homepage.get_todo_count()
+        assert inactive_task_count == "1"
+        self.homepage.check_todo(first_task)
+        assert self.homepage.is_todo_checked(first_task)
+        assert self.homepage.get_todo_count() == "0"
+
+        # Delete a to-do
+        self.homepage.delete_todo(first_task)
+        assert not self.homepage.is_todo_present(first_task)
+
+        # Filtering a to-do is working
+        active_task = "Active Todo"
+        completed_task = "Completed Todo"
+        self.homepage.add_todo(active_task)
+        self.homepage.add_todo(completed_task)
+        assert self.homepage.get_todo_count() == "2"
+        self.homepage.check_todo(completed_task)
+
+        self.homepage.click_active_filter()
+        assert self.homepage.is_todo_present(active_task)
+        assert not self.homepage.is_todo_checked(active_task)
+
+        self.homepage.click_completed_filter()
+        assert self.homepage.is_todo_present(completed_task)
+        assert self.homepage.is_todo_checked(completed_task)
